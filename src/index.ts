5147842b120042e62559fa4f29654a368d6c0b19
lilingfeng <781650249@qq.com>
2021-02-19 15:38:33 +0800

    枚举优化

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,118 @@
 
 
+
+
+//  类型别名
+
+// type Deck = NormalCard[]
+
+// type Color = '♥️' | '♠️' | '♦️' | '♣️';
+
+// type NormalCard = {
+//     color: Color
+//     mark: number
+// }
+
+
+// function createDeck(): Deck {
+
+//     const desk: Deck = []
+//     for (let i = 1; i <= 13; i++) {
+//         desk.push({
+//             color: '♠️',
+//             mark: i
+//         })
+//         desk.push({
+//             color: '♣️',
+//             mark: i
+//         })
+//         desk.push({
+//             color: '♥️',
+//             mark: i
+//         })
+//         desk.push({
+//             color: '♦️',
+//             mark: i
+//         })
+//     }
+//     return desk
+// }
+
+
+
+// function printDeck(deck: Deck) {
+//     let row = '\n'
+//     deck.forEach(card => {
+//         let str = card.color
+//         if (card.mark === 11) {
+//             str += 'J'
+//         } else if (card.mark === 12) {
+//             str += 'Q'
+//         } else if (card.mark === 13) {
+//             str += 'K'
+//         } else {
+//             str += card.mark + row
+//         }
+
+//     })
+//     return deck
+// }
+
+// let card = createDeck()
+
+// console.log(printDeck(card))
+
+
+
+// 枚举
+
 type Deck = NormalCard[]
 
-type Color = '♥️' | '♠️' | '♦️' | '♣️';
-
+enum Color {
+    heart = '♥️',
+    spade = '♠️',
+    club = '♣️',
+    diamond = '♦️'
+}
 type NormalCard = {
     color: Color
-    mark: number
+    mark: Mark
+}
+
+enum Mark {
+    A = 'A',
+    two = '2',
+    three = '3',
+    four = '4',
+    five = '5',
+    six = '6',
+    seven = '7',
+    eight = '8',
+    nine = '9',
+    ten = '10',
+    eleven = '11',
+    twelve = '12'
 }
 
 
-function createDeck(): Deck {
-
-    const desk: Deck = []
-    for (let i = 1; i <= 13; i++) {
-        desk.push({
-            color: '♠️',
-            mark: i
-        })
-        desk.push({
-            color: '♣️',
-            mark: i
-        })
-        desk.push({
-            color: '♥️',
-            mark: i
-        })
-        desk.push({
-            color: '♦️',
-            mark: i
-        })
+function createMark(): Deck {
+    let desk: Deck = []
+    for (let i of Object.values(Mark)) {
+        for (let j of Object.values(Color)) {
+            desk.push({
+                color: j,
+                mark: i
+            })
+        }
     }
     return desk
 }
 
-
-
-function printDeck(deck: Deck) {
-    let row = '\n'
-    deck.forEach(card => {
-        let str = card.color
-        if (card.mark === 11) {
-            str += 'J'
-        } else if (card.mark === 12) {
-            str += 'Q'
-        } else if (card.mark === 13) {
-            str += 'K'
-        } else {
-            str += card.mark + row
-        }
-
-    })
-    return deck
+function printMark(desk: Deck) {
+    desk.forEach(i=>i.color+i.mark)
+    return desk
 }
 
-let card = createDeck()
+let a =createMark()
 
-console.log(printDeck(card))
+console.log(printMark(a))
