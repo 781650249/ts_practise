1dac364addad1e76eab02171fcf228fc8ef540af
lilingfeng <781650249@qq.com>
2021-02-20 14:44:14 +0800

    类

--- src/index.ts
+++ src/index.ts
@@ -36,13 +36,13 @@
 
 
 
-export = {
-    name: 'kevin',
-    sum(a: number, b: number) {
-        return a + b
-    }
+// export = {
+//     name: 'kevin',
+//     sum(a: number, b: number) {
+//         return a + b
+//     }
 
-}
+// }
 
 
 // 接口 
@@ -89,29 +89,57 @@ export = {
 
 
 
-const arr:readonly number[] = [1,2,3,4]
+// const arr:readonly number[] = [1,2,3,4]
 
-const a: ReadonlyArray<number> = [1,2,3,4]
+// const a: ReadonlyArray<number> = [1,2,3,4]
 
 
-type User= {
-    readonly id:string,
-    name:string,
-    age:number,
-    readonly arr:  string[]
-    // readonly arr: readonly string[]
+// type User= {
+//     readonly id:string,
+//     name:string,
+//     age:number,
+//     readonly arr:  string[]
+//     // readonly arr: readonly string[]
 
+// }
+
+// let u:User = {
+//     id:'124',
+//     name:'dee',
+//     age:77,
+//     arr:['dede','erere']
+// }
+
+// u.arr.push('323232')
+// // 数组只读可以添加push,slice等数组方法
+// // 字符串只读不能添加内容成员了
+
+// console.log(u.arr)
+
+
+class User {
+    readonly id: number
+    gender: '男' | '女' = '男'
+    pid?: number
+    constructor(public name: string, public age: number) {
+        this.id = Math.random()
+    }
+    hasUpload: number = 3
+    noUpload: number = 0
+    publish(title: string) {
+        if (this.noUpload < this.hasUpload) {
+            this.noUpload += 1
+            console.log('发布一篇文章'+title)
+        }else{
+            console.log('已发完了')
+        }
+
+    }
 }
+let a = new User('福州人', 18)
 
-let u:User = {
-    id:'124',
-    name:'dee',
-    age:77,
-    arr:['dede','erere']
-}
-
-u.arr.push('323232')
-// 数组只读可以添加push,slice等数组方法
-// 字符串只读不能添加内容成员了
-
-console.log(u.arr)
+console.log(a)
+a.publish('小说')
+a.publish('美女')
+a.publish('游戏')
+a.publish('电影')
